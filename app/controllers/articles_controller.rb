60d2c03dd5563d4e4dce3dcaf13446d0ea73edf6
work <work@Mias-MBP.modem>
2022-05-13 15:48:09 +1000

    last commit pt one

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,28 @@ class ArticlesController < ApplicationController
     def index
         @time = Time.now
         @articles = Article.all
+
+        console
+    end
+
+    def new
+        @article = Article.new
+
+        console
+    end
+
+    def create
+        @article = Article.new(article_params)
+        
+        if @article.save
+        redirect_to articles_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :body, :importance)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get '/articles', to: 'articles#index'
+  # get '/articles', to: 'articles#index'
+  # get '/articles/new', to: 'articles#new'
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
